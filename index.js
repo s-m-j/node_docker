2f7d250788a90497f4bf9bd50a581fece51020e1
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-08 22:07:09 +0300

    sample express app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h3>Привет из express!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log)`listening on port ${port}`;
