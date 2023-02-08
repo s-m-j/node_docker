a406d8bda08e0d17b44bca1a4671a1122af214ea
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-02-08 22:18:14 +0300

    bugfix 1

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log)`listening on port ${port}`;
+app.listen(port, () => console.log(`listening on port ${port}`));
